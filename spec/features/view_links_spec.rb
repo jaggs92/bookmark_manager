0e85a31a7c39f737d903939a14fef5515c445a39
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-25 20:30:24 +0100

    08 - viewing links - completed

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,6 +1,13 @@
 feature 'view links' do
-  scenario 'I would like to see a list of links' do
+  scenario 'I can see a list of links' do
+    Link.create(url: 'http://www.bbc.co.uk', title: 'BBC')
+
     visit '/'
-    expect(page).to have_link("BBC", :href => "www.bbc.co.uk")
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('BBC')
+    end
   end
 end
