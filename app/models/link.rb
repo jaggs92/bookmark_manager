09f58e0d1f3ebca85a14ee60e09662bc07865776
acsauk <acsauk@gmail.com>
2016-07-26 16:01:08 +0100

    heroku troublshooting

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['postgres://vzqscsjqkbfcqw:TEw1AIMJXL336akVV4Wd_3U-xA@ec2-54-243-203-232.compute-1.amazonaws.com:5432/dl6hgb3mqvcst'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
