6d444796f46568afed7797c7720611ea063d108e
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-26 19:57:13 +0100

    Extracted datamapper setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,4 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
+require './app/data_mapper_setup'
 
 class Link
   include DataMapper::Resource
@@ -10,7 +9,7 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+#
+# DataMapper.finalize
+# DataMapper.auto_upgrade!
