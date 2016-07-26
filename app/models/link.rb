40644a6e663fdac385accc8e0796c7b55441382b
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-26 17:15:51 +0100

    remembering to save this time

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,10 +10,3 @@ class Link
   property :url, String
 
 end
-<<<<<<< HEAD
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
-=======
->>>>>>> cc1a99524b2f82b001e0c19860564df2511cd295
